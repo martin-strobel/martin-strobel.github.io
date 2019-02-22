26bc8d0bf0342c4c17c4f92b25b15e496979d1f3
Martin Strobel <martin.strobel@proc-it.com>
2019-02-22 11:16:56 +0100

    add rbc-ie11-bug

---
+++ rbc-ie11-bug/precache-manifest.e17fc3c44f1376f183c83017ccd5c807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89b168b307f92ce3dec",
+    "url": "/rbc-ie11-bug/static/js/main.d89b168b.chunk.js"
+  },
+  {
+    "revision": "315bb990c4dbb529a6c6",
+    "url": "/rbc-ie11-bug/static/js/runtime~main.315bb990.js"
+  },
+  {
+    "revision": "194a169cf2ad035021f0",
+    "url": "/rbc-ie11-bug/static/css/2.8abaecfd.chunk.css"
+  },
+  {
+    "revision": "194a169cf2ad035021f0",
+    "url": "/rbc-ie11-bug/static/js/2.194a169c.chunk.js"
+  },
+  {
+    "revision": "dfbd0ca83bb44fd5c5b3e0268816179f",
+    "url": "/rbc-ie11-bug/index.html"
+  }
+];
---
+++ rbc-ie11-bug/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rbc-ie11-bug/precache-manifest.e17fc3c44f1376f183c83017ccd5c807.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rbc-ie11-bug/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
